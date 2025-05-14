e5ba36f25bebad6552082e8b1c5261524bccad6a
korchel <korchel21@gmail.com>
2025-05-14 14:43:31 +0300

    favicons added

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
+      </head>
       <body className={font.className}>
         <div id="root">
           <Suspense fallback={<Loading />}>
