6cdfdda1329c6109b97ccac88bc6fcda7ff3112c
korchel <korchel21@gmail.com>
2025-05-30 15:08:37 +0300

    content added

--- components/homePage/FitTable/FitTable.tsx
+++ components/homePage/FitTable/FitTable.tsx
@@ -33,29 +33,50 @@ export const FitTable = () => {
         </tr>
         <tr>
           <th scope="row">Легкоходовые</th>
-          <td></td>
-          <td></td>
+          <td>H8/e7, H8/e8, H9/e8, H10/e9</td>
+          <td>E8/h7, E9/h8, E8/h9, E9/h9</td>
         </tr>
         <tr>
           <th scope="row">Широкоходовые</th>
-          <td></td>
-          <td></td>
+          <td>H8/d8, H9/d8, H10/d9, H11/d10</td>
+          <td>D9/h8, D9/h9, D10/H9</td>
         </tr>
         <tr>
           <th rowSpan={3}>С натягом</th>
           <th scope="row">Прессовые тяжелые</th>
-          <td></td>
-          <td></td>
+          <td>H7/u6, H7/x6, H8/u7</td>
+          <td>U7/h6, X7/h6</td>
         </tr>
         <tr>
           <th scope="row">Прессовые средние</th>
+          <td>H7/r6, H7/s6, H7/t6, H8/s7</td>
+          <td>R7/h7, S7/h7, T7/h7</td>
+        </tr>
+        <tr>
+          <th scope="row">Легкопрессовые</th>
+          <td>H6/p5, H7/p6</td>
+          <td>P6/h5, P7/h6</td>
+        </tr>
+        <tr>
+          <th rowSpan={4}>Переходные</th>
+          <th scope="row">Плотные</th>
+          <td></td>
+          <td></td>
+        </tr>{" "}
+        <tr>
+          <th scope="row">Напряженные</th>
           <td></td>
           <td></td>
         </tr>
         <tr>
-          <th scope="row">Легкопрессовые</th>
-          <td></td>
-          <td></td>
+          <th scope="row">Тугие</th>
+          <td>H6/m5, H7/m6, H8/m7</td>
+          <td>M6/h5, M7/h6</td>
+        </tr>
+        <tr>
+          <th scope="row">Глухие</th>
+          <td>H7/n6</td>
+          <td>N7/h6</td>
         </tr>
       </tbody>
     </table>
